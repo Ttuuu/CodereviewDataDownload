11e3ae886465bb7f6d4c18701a5cdda218286519
Ttuuu <756115394@qq.com>
2020-09-02 09:23:46 +0800

    改注释

--- sqlutil.py
+++ sqlutil.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-
-
-
-"""
+"""数据库相关操作"""
 import pymysql
+
 def addaQuestionRecord(owner_id,owner_reputation,view_count,answer_count,score,question_id,title):
-    #print(owner_id,owner_reputation,view_count,answer_count,score,question_id,title)
+    """插入一条问题记录
+        参数
+        -------
+        owner_id : int            # 提问者id
+        owner_reputation : int    # 提问者声誉
+        view_count : int          # 问题访问次数
+        answer_count : int        # 回答个数
+        score : int               # 问题评分
+        question_id : int         # 问题id
+        title : str               # 问题标题
+
+    """
     db = pymysql.connect("localhost","root","123456","codereview" )
     cursor = db.cursor()    
     sql="INSERT INTO QUESTIONS_SET(OWNER_ID,OWNER_REPUTATION,VIEW_COUNT,\
@@ -20,23 +28,12 @@ def addaQuestionRecord(owner_id,owner_reputation,view_count,answer_count,score,q
         print("database error %s"%e)
         db.rollback()
     db.close()
-
-def addaAnswerRecord(owner_id,owner_reputation,view_count,answer_count,score,question_id,title):
-    sql="INSERT INTO QUESTIONS(OWNER_ID,OWNER_REPUTATION,VIEW_COUNT,\
-                                ANSWER_COUNT,SCORE,QUESTION_ID,TITLE)\
-                                VALUES('%d','%d','%d','%d','%d','%d','%s',)"%\
-                                (owner_id,owner_reputation,view_count,answer_count,score,question_id,title)
-    try:
-        cursor.execute(sql)
-        db.commit()
-    except:
-        db.rollback()
     
 def resetVis():
     """网络连接错误时重置数据库"""
     db = pymysql.connect("localhost","root","123456","codereview" )
     cursor = db.cursor()    
-    for i in range(0,20000):
+    for i in range(0,66639):
         sql="select * from questions_set where id=%d and answer_count>0;"%(i)
         try:
             if cursor.execute(sql)==0:
--- test.py
+++ test.py
@@ -1,33 +1,17 @@
-"""
-@author: ttu
-因为校园网半夜断网 所以数据库里设置了vis字段判断该问题是否已经爬过 避免重复访问
-    owner_id：提问者id
-    owner_reputation：提问者声誉
-    view_count：问题访问次数
-    answer_count：回答个数
-    score：问题评分
-    question_id：问题id
-    title：问题标题
-
-网站根据ip来ban请求
-虽然设置了代理 不过没用到
-"""
-from urllib.request import urlopen
-from urllib.request import Request
-import urllib
+"""分页请求stackexchange的api获得所有问题信息"""
+from urllib.error import HTTPError
 import random
 import urllib.request
 import datetime
 import json
 import socket
+from io import BytesIO
+import gzip
 
 import sqlutil
 
-from io import BytesIO
-import gzip
- 
 if __name__ == '__main__':
-    
+    """"""
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36',
                'Content-Type':'application/json',
                'Accept':'application/json',
@@ -46,20 +30,13 @@ if __name__ == '__main__':
         {"http":"http://61.135.186.243:80"},
         {"http":"http://59.120.117.244:80"},
         {"http":"http://218.59.139.238:80"}
-    ]
-    proxy = random.choice(proxy_list)
-    httpproxy_handler = urllib.request.ProxyHandler(proxy)
-    opener = urllib.request.build_opener(httpproxy_handler)
-    res = opener.open(Request("https://www.baidu.com/",headers=headers))
-    print("test end")  #测试一下网络
-    
-    pageindex=1
+    ]   
+    pageindex = 1
     socket.setdefaulttimeout(120)
-    retrytime=5
+    retrytime = 5
     while 1:
-        url = 'https://api.stackexchange.com/questions?page={index}&pagesize=100&site=codereview'.format(index=pageindex)#依次分页访问
-        req = Request(url,headers=headers)
-        testflag=False
+        url = 'http://api.stackexchange.com/questions?page={index}&pagesize=100&site=codereview'.format(index = pageindex)#依次分页访问
+        req = urllib.request.Request(url,headers = headers)
         try:
             for i in range(retrytime):
                 try:
@@ -68,31 +45,29 @@ if __name__ == '__main__':
                     httpproxy_handler = urllib.request.ProxyHandler(proxy)
                     opener = urllib.request.build_opener(httpproxy_handler)
                     res = opener.open(req)
-                    testflag=True
-                    if res.code==200:
+                    if res.code == 200:
                         break
                 except Exception as e:
                     print(e)
-                    print("retrying...",i,"testflag",testflag)
-        except urllib.error.HTTPError:
+                    print("retrying...",i)
+        except HTTPError:
             print("end")
             break
         else:
             response = res.read()
             buff = BytesIO(response)
-            f = gzip.GzipFile(fileobj=buff)
+            f = gzip.GzipFile(fileobj = buff)
             htmls = f.read().decode('utf-8')
-            results=json.loads(htmls)
-
+            results = json.loads(htmls)
             for item in results['items']:
                 if ('user_id' in item['owner']):
-                    owner_id=item['owner']['user_id']
-                    owner_reputation=item['owner']['reputation']
-                    view_count=item['view_count']
-                    answer_count=item['answer_count']
-                    score=item['score']
-                    question_id=item['question_id']
-                    title=item['title']
+                    owner_id=item['owner']['user_id']             # 提问者id
+                    owner_reputation=item['owner']['reputation']  # 提问者声誉
+                    view_count=item['view_count']                 # 问题访问次数
+                    answer_count=item['answer_count']             # 回答个数
+                    score=item['score']                           # 问题评分
+                    question_id=item['question_id']               # 问题id
+                    title=item['title']                           # 问题标题
                     sqlutil.addaQuestionRecord(owner_id,owner_reputation,view_count,answer_count,score,question_id,title)
-            print("read a record",pageindex,"quota remain",results['quota_remaining'])#显示当前ip剩余次数
-            pageindex+=1
+            print("get",pageindex)
+            pageindex += 1
